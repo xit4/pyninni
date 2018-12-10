4d3a47fffaa0ca139a1b213d83b7380889e39287
Vito <vito@igenius.net>
2018-12-10 18:28:00 +0100

    first real commit with basic user registration, login, token refresh

---
+++ constants/strings.py
@@ -0,0 +1,13 @@
+error_user_required = "The username cannot be left blank!"
+error_password_required = "The password cannot be left blank!"
+error_email_required = "The email cannot be left blank!"
+error_username_not_unique = "Username already in use"
+error_email_not_unique = "Email already in use"
+error_user_generic = "An error occurred trying to create a new user"
+error_user_not_found = "User not found"
+error_not_admin = "You need to be an administrator"
+error_patch_other = "You can't patch other users' information"
+
+
+success_user_created = "User created successfully"
+success_user_deleted = "User successfully deleted"
---
+++ models/user.py
@@ -0,0 +1,54 @@
+import sqlite3
+from db import db
+
+
+class UserModel(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(80))
+    email = db.Column(db.String(80), unique=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    last_login = db.Column(db.DateTime())
+    creation_date = db.Column(db.DateTime())
+
+    def __init__(self, username, password, email, creation_date, first_name, last_name, last_login=None):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+        self.last_login = last_login
+        self.creation_date = creation_date
+
+    def json(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "last_login": self.last_login.isoformat() if self.last_login else '',
+            "creation_date": self.creation_date.isoformat() if self.creation_date else ''
+        }
+
+    @classmethod
+    def find_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
+
+    @classmethod
+    def find_by_email(cls, email):
+        return cls.query.filter_by(email=email).first()
+
+    @classmethod
+    def find_by_id(cls, _id):
+        return cls.query.filter_by(id=_id).first()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
