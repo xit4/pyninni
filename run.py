b4ce44e5bf62985f5fdc1670dae0ac6484270111
xit4 <xit4@users.noreply.github.com>
2019-04-05 17:34:32 +0200

    moved init_db to run.py

--- run.py
+++ run.py
@@ -1,7 +1,22 @@
 from app import app
 from db import db, init_db
+from models.nutrient import NutrientModel
+from models.food import FoodModel
 
 db.init_app(app)
+
+
+def init_db():
+    print("Checking if Nutrients are initialized")
+    nutrients = NutrientModel.query.all()
+    if not nutrients:
+        print("Initializing Nutrients")
+        NutrientModel.initialize_nutrients()
+    print("Checking if Foods are initialized")
+    foods = FoodModel.query.all()
+    if not foods:
+        print("Initializing Foods")
+        FoodModel.initialize_foods()
 
 
 @app.before_first_request
